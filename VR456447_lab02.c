829df57bea2f8dfa7d984f4af5227f63467ceafb
angelo marano <53897705+destshadow@users.noreply.github.com>
2020-12-11 14:29:02 +0100

    added file 2

---
+++ VR456447_lab02.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define DIM 8
+
+int main(){
+    int array1[DIM];
+    int array2[DIM];
+    
+
+    for(int i = DIM - 1; i >= 0; i--){
+        scanf("%i", &array1[i]);
+    }
+
+    printf("ciao\n");
+
+    for (int i = DIM - 1; i >= 0; i--){
+        scanf(" %i", &array2[i]);
+    }
+
+    for(int i = 0; i < DIM; i++){
+        if(array1[i] != 0 && array1[i] != 1){
+            return 0;
+        }
+        if(array2[i] != 0 && array2[i] != 1){
+            return 0;
+        }
+    }
+
+
+
+
+    return 0;
+}
