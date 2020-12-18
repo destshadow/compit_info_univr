7c9bc2f5fc0ec513eaa3ed71ced0a0363a6047b3
angelo marano <53897705+destshadow@users.noreply.github.com>
2020-12-18 12:43:25 +0100

    fixxed

--- VR456447_lab02.c
+++ VR456447_lab02.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-#define DIM 8
+#define DIM 4
 
 int main(){
     int array1[DIM];
@@ -13,37 +13,49 @@ int main(){
         scanf("%i", &array1[i]);
     }
 
-    printf("ciao\n");
-
     for (int i = DIM - 1; i >= 0; i--){
         scanf(" %i", &array2[i]);
     }
 
     for(int i = 0; i < DIM; i++){
         if(array1[i] != 0 && array1[i] != 1){
+            printf("Errore nell'inserimento dei valori!\n");
             return 0;
         }
         if(array2[i] != 0 && array2[i] != 1){
+            printf("Errore nell'inserimento dei valori!\n");
             return 0;
         }
     }
 
-    for(int i = DIM; i > 0; i--){
-        resto = 0;
-        if(array2[i] == 1 && array2[i] == 1){
-            array3[i] = 0;
-            resto++;
+    for(int i = DIM - 1; i >= 0; i--){
+        if(array1[i] == 1 && array2[i] == 1){
+            if(resto == 0){
+                array3[i] = 0;
+                resto++;
+            }else{
+                array3[i] = 1;
+                resto++;
+            }
         }else{
-            array3[i] = array1[i] + array2[i] + resto;
+            if(resto == 0){
+                array3[i] = array1[i] + array2[i];
+            }else if(array1[i] == 1 || array2[i] == 1){
+                array3[i] = 1;
+                resto = 0;
+            }
         }
     }
+
+    for(int i = 0; i < 7; i++){
+        printf("%c", '-');
+    }
+
+    printf("\n");
 
     for(int i = 0; i < DIM; i++){
         printf("%i ", array3[i]);
     }
-    printf("\nresto: %i", resto);
-
-
 
     return 0;
 }
